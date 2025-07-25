8e1cbbce04e6e623e03ab1360a3850e9697d6288
truedest1ny <olegkozelres@gmail.com>
2025-07-25 12:23:44 +0300

    The controller that handles errors at the servlet container level has been renamed

--- src/main/java/com/kozel/bookstore/web/controller/ServletErrorHandler.java
+++ src/main/java/com/kozel/bookstore/web/controller/ServletErrorHandler.java
@@ -6,7 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class ErrorController {
+public class ServletErrorHandler {
 
     @RequestMapping("/error")
     public String handleError (HttpServletRequest request, Model model){
